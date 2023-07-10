d3032e3da02a6c83fd73225e96ea9a3b36fa9329
PEDROGRUNWALD97 <PEDRO.GRUNWALD@GMAIL.COM>
2023-07-10 20:23:24 -0300

    feat: Component HeaderDetails

--- src/app/movieDetail/page.tsx
+++ src/app/movieDetail/page.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import ContainerMovies from '../components/ContainerMovies'
+import Footer from '../components/Footer'
+import HeaderDetails from '../components/HeaderDetails'
 
 const MovielDetail = () => {
   return (
-    <div>MovielDetail</div>
+    <>
+      <HeaderDetails />
+      <ContainerMovies />
+      <Footer />
+
+    </>
   )
 }
 
